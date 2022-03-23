670880e4c55809c8da628cba0909ff300789dccd
saritha devops <saritha.devops@gmail.com>
2022-03-23 13:14:18 -0400

    deleted some files from monu_terraform

--- main.tf
+++ main.tf
@@ -8,34 +8,38 @@ resource "azurerm_resource_group" "containerrg" {
   location = "eastus"
 }
 
-resource "azurerm_storage_account" "mystoreacc" {
+resource "azurerm_storage_account" "test" {
   name                     = "mystoreacc"
   resource_group_name      = azurerm_resource_group.containerrg.name
   location                 = azurerm_resource_group.containerrg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+tags = {
+environment = "development"
+}
 }
 
 resource "azurerm_storage_container" "azuresc" {
   name                  = "azuresc"
-  storage_account_name  = azurerm_storage_account.mystoreacc.name
+  storage_account_name  = azurerm_storage_account.test.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_share" "azuress" {
   name                 = "azuress"
-  storage_account_name = azurerm_storage_account.mystoreacc.name
+  storage_account_name = azurerm_storage_account.test.name
   quota                = 50
 }
 
 resource "azurerm_storage_queue" "azuresq" {
   name                 = "azuresq"
-  storage_account_name = azurerm_storage_account.mystoreacc.name
+  storage_account_name = azurerm_storage_account.test.name
 }
 
 resource "azurerm_storage_table" "azurest" {
   name                 = "azurest"
-  storage_account_name = azurerm_storage_account.mystoreacc.name
+  storage_account_name = azurerm_storage_account.test.name
 }
 
 resource "azurerm_container_registry" "ronu" {
